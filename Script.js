3088c72b25ae141842c22d01a0d26d66943039fa
KristanLloyd <kristanlloydbalanga@gmail.com>
2023-10-12 09:19:30 +0800

    reduced the codes for P element

--- Script.js
+++ Script.js
@@ -1,8 +1,8 @@
-let stars = document.getElementById('stars');
+			let stars = document.getElementById('stars');
 			let moon = document.getElementById('moon');
 			let mountains_behind = document.getElementById('mountains_behind');
 			let mountains_front = document.getElementById('mountains_front');
-
+			
 			window.addEventListener('scroll', function(){
 				let value = window.scrollY;
 				stars.style.left = value * 0.25 + 'px';
